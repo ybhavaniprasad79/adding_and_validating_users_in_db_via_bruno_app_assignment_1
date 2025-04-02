a96c6ad908910f20495dcac5736eb6a85a70b677
tony <ybhavaniprasad79@gmail.com>
2025-04-02 16:48:42 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { resolve } = require('path');
+const {connection} =require("./connection")
+const {Model} =require('./Model')
+const bcrypt=require('bcrypt');
+const { error } = require('console');
 
 const app = express();
 const port = 3010;
+app.use(express.json())
 
 app.use(express.static('static'));
 
@@ -10,6 +15,39 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+
+app.post('/signup',async(req,res)=>{
+
+  const {username,mail,password}=req.body;
+  
+  bcrypt.hash(password,6,async(error,hash)=>{
+
+    if (error){
+      res.status(500).json({message:"Server Error"})
+    }
+
+    let newuser=new Model({username,mail,password:hash})
+
+    await newuser.save()
+    res.status(200).json({ status: true, message: "registration sucessfull" })
+    
+  })
+
+})
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+  try {
+
+
+    console.log(`Example app listening at http://localhost:${port}`);
+
+    connection
+    
+  } catch (error) {
+    console.log(error)
+  }
+  
 });
